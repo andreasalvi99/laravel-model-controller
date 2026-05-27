87abd2645b253bd3f7935e1bbee9cf19973df8b9
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-27 11:46:07 +0200

    print as many card as movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,14 @@
 </head>
 <body>
     Ciao
-
-    @php
-        // dd($movies);
-    @endphp
+    @foreach ($movies as $movie)
+        <div class="card text-bg-light mb-3">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+                <h5 class="card-title">Light card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            </div>
+        </div>
+    @endforeach
 </body>
 </html>
