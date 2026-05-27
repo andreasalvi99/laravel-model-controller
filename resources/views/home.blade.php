1d4d2e3ae54a874d3a08cd900b45dc397b978abd
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-27 15:08:43 +0200

    add layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/sass/app.scss', "resources/js/app.js"])
-    <title>Document</title>
-</head>
-<body>
-    {{-- @php
-        dd($movies)
-    @endphp --}}
+@extends('layouts.master')
+
+@section('movies-list')
     <div class="container p-4">
         <div class="row row-cols-3 g-4">
             @foreach ($movies as $movie)
@@ -28,5 +18,4 @@
             @endforeach
         </div>
     </div>
-</body>
-</html>
+    @endsection
