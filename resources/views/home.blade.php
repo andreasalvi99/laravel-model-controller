68d978a45b161285906e891d07fea625d049871d
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-27 11:47:09 +0200

    add row cols

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,18 @@
     <title>Document</title>
 </head>
 <body>
-    Ciao
-    @foreach ($movies as $movie)
-        <div class="card text-bg-light mb-3">
-            <div class="card-header">Header</div>
-            <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <div class="row row-cols-3">
+        @foreach ($movies as $movie)
+            <div class="col">
+                <div class="card text-bg-light mb-3">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Light card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    </div>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </body>
 </html>
