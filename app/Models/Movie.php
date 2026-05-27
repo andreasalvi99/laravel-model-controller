2cdeca99a98f98190d38ffdb5d95559500f4dd7d
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-27 11:24:12 +0200

    create model

---
+++ app/Models/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    public $id;
+    public $name;
+    public $release_date;
+    public $rating;
+
+}
